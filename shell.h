e41d6e713c016ecacacb7da6d1634c7aac22658b
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-04-03 00:16:41 -0700

    Comment

--- shell.h
+++ shell.h
@@ -23,7 +23,7 @@
 #define CMD_AND 2
 #define CMD_CHAIN 3
 
-/* This is for convert_number() - defined constants */
+/* This is for convert_number() - constants defined */
 #define CONVERT_LOWERCASE 1
 #define CONVERT_UNSIGNED 2
 
